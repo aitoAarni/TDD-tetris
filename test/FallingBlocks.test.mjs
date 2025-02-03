bb9336cc1c40174bb5e704f3a93e4710679e78b4
aitoAarni <aarni.rechardt@gmail.com>
2025-02-03 22:34:58 +0100

    npm install, and mjs -> ts

--- test/FallingBlocks.test.mjs
+++ test/FallingBlocks.test.mjs
@@ -1,6 +1,6 @@
 import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
-import { Board } from "../src/Board.mjs";
+import { Board } from "../src/Board.js";
 
 describe("Falling blocks", () => {
   let board;
