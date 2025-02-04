154eebd7beffe3f5633eaef18293fdfa5c9a9706
aitoAarni <aarni.rechardt@gmail.com>
2025-02-04 15:58:10 +0100

    add new test

--- test/FallingBlocks.test.mjs
+++ test/FallingBlocks.test.mjs
@@ -90,7 +90,7 @@ describe("Falling blocks", () => {
       board.tick();
     });
 
-    test.skip("it is still moving on the row above the other block", () => {
+    test("it is still moving on the row above the other block", () => {
       expect(board.toString()).to.equalShape(
         `...
          .Y.
