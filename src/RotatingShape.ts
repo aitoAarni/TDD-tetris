ec4680d3f1112a2dec696a4b954c13394902dde3
aitoAarni <aarni.rechardt@gmail.com>
2025-02-04 22:51:26 +0100

    refactor rest of class naming

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -1,7 +1,7 @@
 export class RotatingShape {
-  shape2: string[][];
-  constructor( shape2: string[][] = [[""]]) {
-    this.shape2 = shape2;
+  shape: string[][];
+  constructor( shape: string[][] = [[""]]) {
+    this.shape = shape;
   }
   static fromString(str: string) {
     const shapeStr = str.replace(/\s/g, "");
@@ -16,7 +16,7 @@ export class RotatingShape {
     return new RotatingShape( shape);
   }
   toString() {
-    const shapeString = this.shape2.map((row) => {
+    const shapeString = this.shape.map((row) => {
         return row.join("")
     }).join("\n") + "\n"
     return shapeString;
