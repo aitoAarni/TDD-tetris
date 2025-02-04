0d5950e0c5a8992000ddc37588923e9044ae6b1b
aitoAarni <aarni.rechardt@gmail.com>
2025-02-04 22:42:06 +0100

    refactor a new type for shape

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -7,6 +7,16 @@ export class RotatingShape {
   }
   static fromString(str: string) {
     const shape = str.replace(/[ \t]/g, "") + "\n";
+    const shape2Str = str.replace(/\s/g, "");
+    const shape2: string[][] = []
+    for (let i = 0; i<3; i++){
+        const newRow = []
+        for (let j = 0; j < 3; j++){
+            newRow.push(shape2Str[i*3+j])
+        }
+        shape2.push(newRow)
+    }
+    console.log(shape2)
     return new RotatingShape(shape);
   }
   toString() {
