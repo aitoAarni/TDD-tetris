0e47a49ea70e5d538692f223bd8d1e9207ed08c5
aitoAarni <aarni.rechardt@gmail.com>
2025-02-04 22:50:27 +0100

    refactor fromString method variable names

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -4,17 +4,16 @@ export class RotatingShape {
     this.shape2 = shape2;
   }
   static fromString(str: string) {
-    const shape = str.replace(/[ \t]/g, "") + "\n";
-    const shape2Str = str.replace(/\s/g, "");
-    const shape2: string[][] = []
+    const shapeStr = str.replace(/\s/g, "");
+    const shape: string[][] = []
     for (let i = 0; i<3; i++){
         const newRow = []
         for (let j = 0; j < 3; j++){
-            newRow.push(shape2Str[i*3+j])
+            newRow.push(shapeStr[i*3+j])
         }
-        shape2.push(newRow)
+        shape.push(newRow)
     }
-    return new RotatingShape( shape2);
+    return new RotatingShape( shape);
   }
   toString() {
     const shapeString = this.shape2.map((row) => {
