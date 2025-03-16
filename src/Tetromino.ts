863b59a88ecbf14f3673d126926fd413e6afea42
aitoAarni <aarni.rechardt@gmail.com>
2025-03-16 17:45:06 +0200

    test rotateLeft passes

--- src/Tetromino.ts
+++ src/Tetromino.ts
@@ -11,6 +11,13 @@ class I_shape {
         return this.shape.rotateRight()
     }
 
+    rotateLeft() {
+        const newShape = this.shape.rotateLeft()
+        const temp = newShape.shape[newShape.shape.length-1]
+        newShape.shape[newShape.shape.length-1] = newShape.shape[0]
+        newShape.shape[0] = temp
+        return newShape
+    }
     toString(){
         return this.shape.toString()
     }
