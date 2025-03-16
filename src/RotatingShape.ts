15a2b01f7b3010c5f3e37205301177143d81fe57
aitoAarni <aarni.rechardt@gmail.com>
2025-03-16 18:59:03 +0200

    add size to RotatingShape

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -1,7 +1,9 @@
 export class RotatingShape {
   shape: string[][];
+  size: number
   constructor(shape: string[][] = [[""]]) {
     this.shape = shape;
+    this.size = shape.length
   }
   static fromString(str: string) {
     const shape = str.split(/\s/g).filter(item => item.length>0).map(row => row.split(""));
