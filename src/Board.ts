4b59464c609eb0b77de3ef01e46bf458ea81469f
aitoAarni <aarni.rechardt@gmail.com>
2025-03-16 18:26:18 +0200

    change board creation

--- src/Board.ts
+++ src/Board.ts
@@ -7,11 +7,7 @@ export class Board {
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
-    this.board = [
-      [".", ".", "."],
-      [".", ".", "."],
-      [".", ".", "."],
-    ];
+    this.board = new Array(height).fill(null).map(() => new Array(width).fill("."))
     this.tetrominoFalling = false;
     this.fallingTetrominoRow = 0;
   }
