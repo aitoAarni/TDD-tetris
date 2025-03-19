b44c01dd6d35324eaea9bf3c5c254a672adf2721
aitoAarni <aarni.rechardt@gmail.com>
2025-03-19 18:44:49 +0200

    format

--- src/Board.ts
+++ src/Board.ts
@@ -76,10 +76,10 @@ export class Board {
 
       if (block === ".") return;
       if (
-          columnIndex < (this.fallingTetromino as TetrominoShape).size - 1 &&
-          this.fallingTetromino?.rotatingShape.shape[rowIndex][columnIndex + 1] !== "."
-        )
-          return;
+        columnIndex < (this.fallingTetromino as TetrominoShape).size - 1 &&
+        this.fallingTetromino?.rotatingShape.shape[rowIndex][columnIndex + 1] !== "."
+      )
+        return;
           if (
           this.tetrominoStartColumn + columnIndex < this.width - 1 &&
           this.board[rowStart + rowIndex][this.tetrominoStartColumn + columnIndex + 1] !== "."
