02dbea822ca79c96cfc09c17757a4150e1a0d0ae
aitoAarni <aarni.rechardt@gmail.com>
2025-03-21 23:19:28 +0200

    refactor

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -1,14 +1,4 @@
 export class RotatingShape {
-  shape: string[][];
-  size: number
-  constructor(shape: string[][] = [[""]]) {
-    this.shape = shape;
-    this.size = shape.length
-  }
-  static fromString(str: string) {
-    const shape = str.split(/\s/g).filter(item => item.length>0).map(row => row.split(""));
-    return new RotatingShape(shape);
-  }
 
 
 }
