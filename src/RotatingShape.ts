1374034710d2a5365a75f1451baad0811102032d
aitoAarni <aarni.rechardt@gmail.com>
2025-03-21 23:19:21 +0200

    refactor

--- src/RotatingShape.ts
+++ src/RotatingShape.ts
@@ -9,15 +9,6 @@ export class RotatingShape {
     const shape = str.split(/\s/g).filter(item => item.length>0).map(row => row.split(""));
     return new RotatingShape(shape);
   }
-  
-  rotateRight(){
-    const rotatedShape = new Array(this.shape.length).fill(null).map(() => new Array(this.shape.length).fill(""))
-    for (let i = 0; i<this.shape.length; i++){
-      this.shape[i].forEach((item, index) => rotatedShape[index][this.shape.length-1-i] = item)
-
-    }
-    return new RotatingShape(rotatedShape)
-  }
 
 
 }
