fa0ed296ba9178ca2738769986b28ef496f4a127
aitoAarni <aarni.rechardt@gmail.com>
2025-03-22 17:58:03 +0200

    format

--- src/ShuffleBag.ts
+++ src/ShuffleBag.ts
@@ -2,12 +2,13 @@ import { TetrominoShape } from "./Tetromino";
 
 export default class ShuffleBag {
   tetrominoes: TetrominoShape[];
-  tetrominoCount: number
-  indexes: number[]
+  tetrominoCount: number;
+  indexes: number[];
   constructor(tetrominoes: TetrominoShape[]) {
     this.tetrominoes = tetrominoes;
-    this.tetrominoCount = tetrominoes.length
-    this.indexes = Array(this.tetrominoCount * 4).fill(0).map((_, index) => (index % this.tetrominoCount))
-    
+    this.tetrominoCount = tetrominoes.length;
+    this.indexes = Array(this.tetrominoCount * 4)
+      .fill(0)
+      .map((_, index) => index % this.tetrominoCount);
   }
 }
