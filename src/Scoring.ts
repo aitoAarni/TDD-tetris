eeef4f9b6c94566b72386254f3b9ceee63d6796c
aitoAarni <aarni.rechardt@gmail.com>
2025-03-22 03:00:57 +0200

    format

--- src/Scoring.ts
+++ src/Scoring.ts
@@ -1,16 +1,14 @@
-
 export default class Scoring {
-    totalScore: number
-    constructor() {
-        this.totalScore = 0
-    }
+  totalScore: number;
+  constructor() {
+    this.totalScore = 0;
+  }
 
-addScore(score: number) {
+  addScore(score: number) {
     this.totalScore += score;
-    }
-    calculateScore(linesRemoved: number) {
+  }
+  calculateScore(linesRemoved: number) {
     const scores = [0, 40, 100, 300, 1200];
     return scores[linesRemoved];
-    }
+  }
 }
-    
