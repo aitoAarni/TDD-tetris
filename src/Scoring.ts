50adbd5080138ae514fac9eb56ed9a7097675fc9
aitoAarni <aarni.rechardt@gmail.com>
2025-03-22 02:53:45 +0200

    change

---
+++ src/Scoring.ts
@@ -0,0 +1,7 @@
+
+export default class Scoring {
+    totalScore: number
+    constructor() {
+        this.totalScore = 0
+    }
+}
---
+++ test/Scoring.test.mjs
@@ -0,0 +1,17 @@
+import { beforeEach, describe, test, vi, expect } from "vitest";
+import Scoring from "../src/Scoring";
+
+// Has an initial score of 0
+// score can be increased
+// update function increases score
+// score is increased when rows/lines are removed
+
+describe("Scoring system test", () => {
+  let scoring;
+  beforeEach(() => {
+    scoring = new Scoring();
+  });
+  test("Scoring has a total score", () => {
+    expect(scoring.totalScore).toBe(0);
+  });
+});
